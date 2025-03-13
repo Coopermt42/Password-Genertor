b98a748bf75d76df190298e515741d655fcda0f8
Coopermt42 <140448571+Coopermt42@users.noreply.github.com>
2025-03-13 12:59:42 -0500

    Add files via upload

--- base.js
+++ base.js
@@ -1,16 +1,17 @@
-function generatePassword(len) {
-    var text = '';
+const display = document.getElementById('display');
 
-    var charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#$*';
-
-    for (var i = 0; i < len; i++) 
-        text += charset.charAt(Math.floor(Math.random() * charset.length));
-
-    return text;
+function showOnDisplay(letter) {
+    display.value += letter;
 }
 
-function showPassword() {
-    var newElement = document.createElement('p');
-    newElement.append(generatePassword(10));
-    document.getElementById("passwords").append(newElement);
+function clearDisplay() {
+    display.value = '';
+}
+
+function calculate() {
+    display.value = eval(display.value);
+}
+
+function backspace() {
+    display.value = display.value.slice(0, -1);
 }
